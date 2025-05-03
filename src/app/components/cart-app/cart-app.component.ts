e30eba421ce4d1b480d1f6118aabbaabd898bc5d
LuisWork <luiszambrano.work@gmail.com>
2025-05-02 20:13:01 -0400

    Implementando Bootstrap en el findAll

--- src/app/components/cart-app/cart-app.component.ts
+++ src/app/components/cart-app/cart-app.component.ts
@@ -1,17 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ProductService } from '../../services/product.service';
 import { Product } from '../../models/product';
+import { CatalogComponent } from '../catalog/catalog.component';
 
 @Component({
   selector: 'app-cart-app',
-  imports: [],
+  imports: [CatalogComponent],
   templateUrl: './cart-app.component.html',
   styleUrl: './cart-app.component.css'
 })
-export class CartAppComponent {
+export class CartAppComponent implements OnInit{
 
   products!: Product[];
 
   constructor(private service: ProductService) {}
+
+  ngOnInit(): void {
+    this.products = this.service.findAll();
+  }
 
 }
