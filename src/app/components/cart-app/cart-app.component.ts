a68055a96dd947970f73a07b56cf1f9d817f4905
LuisWork <luiszambrano.work@gmail.com>
2025-05-05 22:43:53 -0400

    Implementando openCloseCart

--- src/app/components/cart-app/cart-app.component.ts
+++ src/app/components/cart-app/cart-app.component.ts
@@ -16,11 +16,13 @@ export class CartAppComponent implements OnInit {
   products!: Product[];
   items: CartItem[] = [];
   total: number = 0;
+  showCart: boolean = false;
 
   constructor(private service: ProductService) { }
 
   ngOnInit(): void {
     this.products = this.service.findAll();
+    this.items = JSON.parse(sessionStorage.getItem('cart')!) || [];
     this.calculateTotal();
   }
 
@@ -37,14 +39,24 @@ export class CartAppComponent implements OnInit {
       this.items = [... this.items, { product: { ...product }, quantity: 1 }];
     }
     this.calculateTotal();
+    this.saveSession();
   }
 
   onDeleteCart(id: number): void {
     this.items = this.items.filter(item => item.product.id != id);
     this.calculateTotal();
+    this.saveSession();
   }
 
   calculateTotal(): void {
     this.total = this.items.reduce((accumulator, item) => accumulator + (item.quantity * item.product.price), 0);
+  }
+
+  saveSession(): void {
+    sessionStorage.setItem('cart', JSON.stringify(this.items));
+  }
+
+  openCart(): void {
+    this.showCart = !this.showCart;
   }
 }
