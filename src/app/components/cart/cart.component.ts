4354584a7b9a846f50022fac7ba199797c34aab8
LuisWork <luiszambrano.work@gmail.com>
2025-05-05 22:02:58 -0400

    Implementando onDeleteCart

--- src/app/components/cart/cart.component.ts
+++ src/app/components/cart/cart.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { CartItem } from '../../models/cartItem';
 
 @Component({
@@ -11,4 +11,9 @@ export class CartComponent {
 
   @Input() items: CartItem[] = [];
 
+  @Output() idProductEventEmitter: EventEmitter<number> = new EventEmitter();
+
+  onDeleteCart(id: number): void {
+    this.idProductEventEmitter.emit(id);
+  }
 }
