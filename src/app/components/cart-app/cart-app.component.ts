5de7c07e86342c67483c608179c71f78c98d1b2b
LuisWork <luiszambrano.work@gmail.com>
2025-05-10 19:37:34 -0400

    Implementando itemsReducer

--- src/app/components/cart-app/cart-app.component.ts
+++ src/app/components/cart-app/cart-app.component.ts
@@ -21,25 +21,15 @@ export class CartAppComponent implements OnInit {
 
   ngOnInit(): void {
     this.items = JSON.parse(sessionStorage.getItem('cart') || '[]');
-    this.calculateTotal();
+    //this.calculateTotal();
     this.onDeleteCart();
     this.onAddCart();
   }
 
   onAddCart(): void {
     this.sharingDataService.productEventEmitter.subscribe(product => {
-      const hasItem = this.items.find(item => item.product.id === product.id);
-      if (hasItem) {
-        this.items = this.items.map(item => {
-          if (item.product.id === product.id) {
-            return { ...item, quantity: item.quantity + 1 }
-          }
-          return item;
-        })
-      } else {
-        this.items = [... this.items, { product: { ...product }, quantity: 1 }];
-      }
-      this.calculateTotal();
+
+      //this.calculateTotal();
       this.saveSession();
       this.router.navigate(['/cart'], {
         state: { items: this.items, total: this.total }
@@ -57,11 +47,7 @@ export class CartAppComponent implements OnInit {
         confirmButtonText: "Delete",
       }).then((result) => {
         if (result.isConfirmed) {
-          this.items = this.items.filter(item => item.product.id != id);
-          if (this.items.length == 0) {
-            sessionStorage.removeItem('cart');
-          }
-          this.calculateTotal();
+          //this.calculateTotal();
           this.saveSession();
           Swal.fire("The item has been successfully deleted", "", "success");
           this.router.navigateByUrl('/', { skipLocationChange: true }).then(() => {
@@ -74,10 +60,6 @@ export class CartAppComponent implements OnInit {
         }
       });
     });
-  }
-
-  calculateTotal(): void {
-    this.total = this.items.reduce((accumulator, item) => accumulator + (item.quantity * item.product.price), 0);
   }
 
   saveSession(): void {
